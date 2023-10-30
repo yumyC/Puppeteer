16bc737e8f010a017c6f1a45a5b9bbaf23f82d88
yummyc <yummyc@ciant.com>
2023-10-30 17:30:56 +0800

    Code optimization

--- puppeteer pagediff/main.js
+++ puppeteer pagediff/main.js
@@ -1,7 +1,7 @@
 const puppeteer = require('puppeteer');
 const { PNG } = require('pngjs');
 const pixelmatch = require('pixelmatch');
-const fs = require('fs');
+const fs = require('fs').promises;
 
 const maxAttempts = 10;
 const Attributes = process.argv.splice(2);
@@ -9,19 +9,19 @@ const viewport = {width: 1980, height: 1980};
 const foldNames = ['diff-Images', 'diff-screenshot', 'diff-errorlog'];
 const imgPrefix = '../../';
 
-const createFolders = (() => {
-  foldNames.forEach(foldName => {
+const createFolders = async () => {
+  for (const foldName of foldNames) {
     const folderPath = `${imgPrefix}${foldName}`;
-    if (!fs.existsSync(folderPath)) {
+    try {
+      await fs.mkdir(folderPath);
       console.log(`create ${foldName} Folder`);
-      fs.mkdirSync(folderPath);
-    } else {
+    } catch (error) {
       console.log(`${foldName} Folder already exists`);
     }
-  });
-});
+  }
+};
 
-const getConfigData = (() => {
+const getConfigData = async () => {
   let urls = [];
   if (!Attributes.length) {
     console.log(`missing instruction. \r\n Please refer to the readme file`);
@@ -31,68 +31,55 @@ const getConfigData = (() => {
     urls = [{
       "beforeUrl": Attributes[1],
       "afterUrl": Attributes[2]
-    }]
+    }];
   }
   if (Attributes[0] === 'batch') {
     const configUrl = Attributes[1];
-    const rawdata = fs.readFileSync(configUrl);
-    urls = JSON.parse(rawdata).batchData;
+    const rawdata = await fs.readFile(configUrl);
+    urls = JSON.parse(rawdata);
   }
   return urls;
-});
+};
 
-async function processUrl(browser, url, i) {
+const processUrl = async (browser, url, i) => {
   const page = await browser.newPage();
   await page.setViewport(viewport);
   for (let j = 0; j < maxAttempts; j++) {
     try {
-      console.log(`visiting${url.beforeUrl}...`);
+      console.log(`visiting ${url.beforeUrl}...`);
       await page.goto(url.beforeUrl, {waitUntil: 'networkidle2', timeout: 10000 });
       const screenshot = await page.screenshot({ path: `${imgPrefix}${foldNames[1]}/before${i}.png` });
-      console.log(`visiting${url.afterUrl}...`);
-      // 自定义更改dom结构
-      // await page.waitForSelector('#disclaimer-bar');
-      // await page.evaluate(() => {
-      //   const disclaimer = document.querySelector('#disclaimer-bar');
-      //   if (disclaimer) {
-      //     disclaimer.parentNode.removeChild(disclaimer);
-      //   }
-      //   const header = document.querySelector('.has-disclaimer');
-      //   if (header) {
-      //   header.style.paddingTop = '0px';
-      //   }
-      // });
+      console.log(`visiting ${url.afterUrl}...`);
       await page.goto(url.afterUrl, {waitUntil: 'networkidle2', timeout: 10000 });
       const screenshot2 = await page.screenshot({ path: `${imgPrefix}${foldNames[1]}/after${i}.png` });
-      await diffImage(screenshot, screenshot2, `${url.beforeUrl} and ${url.afterUrl}`);
-      break; // 如果没有错误，那么退出循环
+      await diffImage(screenshot, screenshot2, `${url.beforeUrl}_diff_${url.afterUrl}`);
+      break;
     } catch (e) {
       console.warn(`Attempt ${j + 1} failed for ${url.beforeUrl} and ${url.afterUrl}: ${e}`);
-      if (j === maxAttempts - 1) { // 如果这是最后一次尝试，那么抛出错误
-        fs.appendFileSync(`${imgPrefix}${foldNames[2]}/error.json`, JSON.stringify(url));
+      if (j === maxAttempts - 1) {
+        await fs.appendFile(`${imgPrefix}${foldNames[2]}/error.json`, JSON.stringify(url));
         throw new Error(`Failed to process URL after ${maxAttempts} attempts`);
       }
     }
   }
   await page.close();
-}
-function diffImage(screenshot1, screenshot2, url) {
+};
+
+const diffImage = async (screenshot1, screenshot2, url) => {
   console.log(`open compare ${url}`);
   const img1 = PNG.sync.read(screenshot1);
   const img2 = PNG.sync.read(screenshot2);
-
   const diff = new PNG({ width: img1.width, height: img1.height });
-
   const numDiffPixels = pixelmatch(img1.data, img2.data, diff.data, img1.width, img1.height, { threshold: 0.1 });
-
-  fs.writeFileSync(`${imgPrefix}${foldNames[0]}/${url.replace(/\//g, "_")}.png`, PNG.sync.write(diff));
-
+  await fs.writeFile(`${imgPrefix}${foldNames[0]}/${ url.replace(/https?:\/\//g, '').replace(/\//g, "_")}.png`, PNG.sync.write(diff));
   console.log(`Number of difference pixels: ${numDiffPixels}`);
-}
+};
+
 console.log('turning on...');
+
 (async () => {
   const urls = await getConfigData();
-  if(!urls?.length) {
+  if(!urls.length) {
     console.log(`missing instruction. \r\n Please refer to the readme file`);
     return;
   }
